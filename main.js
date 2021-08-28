22799ca187ce6b46ae531e242367b5f4c53c022a
werty-lang <84279588+werty-lang@users.noreply.github.com>
2021-08-28 17:25:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX =0;
+noseY =0;
+function preload(){
+clown_nose= loadImage("https://postimg.cc/LqBfJJq7");
+} 
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+function draw(){
+image(video, 0, 0, 300,300);
+
+//fill(255,0,0);
+
+//stroke(255,0,0);
+//circle(noseX,noseY,20);
+image(clown_nose,noseX-15,noseY-15,30,30)
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+function modelLoaded(){
+    console.log("poseNet instaled")
+}
+
+function gotPoses(results){
+if(results.length > 0){
+  console.log(results);
+   noseX= results[0].pose.nose.x;
+    noseY= results[0].pose.nose.y;
+    console.log("nose x ="+noseX);
+    console.log("nose y ="+ noseY);
+}
+}
